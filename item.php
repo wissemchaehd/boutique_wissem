fc73b4c3220f23e5925d092d8aa3b7cffabce404
Wissem CHAHED <wissem.chahed@le-campus-numerique.fr>
2021-05-20 09:54:44 +0200

    variable php

---
+++ item.php
@@ -0,0 +1,8 @@
+<?php
+$nom=" votre avion";
+$prix=" à 300e par journéé";
+$image= "https://www.site-annonce.fr/sh-img/prix-location-avion-prive-1.jpg" ;
+echo "$nom  ";
+echo " $prix ";
+echo '<img src="'.$image.'" alt="votre avion " width="200">';
+?>
