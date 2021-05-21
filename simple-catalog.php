ed6401b9be1f02f01854c99ed7ad8cff9525b8db
wissem <wissem-chahed@le-campus-numerique.fr>
2021-05-21 14:29:12 +0200

    boucle while

--- simple-catalog.php
+++ simple-catalog.php
@@ -3,6 +3,12 @@
 $produits = ['avion1', 'avion2', 'helicoptere', 'helicoptere2','jet'];
 sort($produits);
 
-foreach ($produits as $prod) {
-    echo "$prod.<br/>";
+$i = 0;
+$max = count( $produits );
+
+while( $i < $max )
+{
+    echo $produits[ $i ] . '<br />';
+    $i++;
 }
+?>
