e4ef83823941805cd524e99975e0fac963c79a6e
wissem <wissem-chahed@le-campus-numerique.fr>
2021-05-21 14:14:38 +0200

    boucle foreach

--- simple-catalog.php
+++ simple-catalog.php
@@ -2,8 +2,7 @@
 
 $produits = ['avion1', 'avion2', 'helicoptere', 'helicoptere2','jet'];
 sort($produits);
-for ($i = 0; $i < count($produits); $i++) {
-    $prod = $produits[$i];
-    echo ./ $prod . '<br>';
-}
 
+foreach ($produits as $prod) {
+    echo "$prod.<br/>";
+}
