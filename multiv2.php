0ebd2a4140d4fee2ff558c446610ef521a377d10
wissem <wissem-chahed@le-campus-numerique.fr>
2021-05-24 11:14:20 +0200

    boucle foreach

---
+++ multiv2.php
@@ -0,0 +1,37 @@
+<?php
+$array= [
+
+    'produit_1' => [
+        'nom' =>"avion1",
+        'prix' => 400,
+        'poid' => 15,
+        'remise' => null,
+        'photo' => "https://www.site-annonce.fr/sh-img/prix-location-avion-prive-1.jpg"
+    ],
+
+    'produit_2'=> [
+        'nom' =>"avion2",
+        'prix' => 300,
+        'poid'=> 10,
+        'remise' => null,
+        'photo' => "https://f1i.autojournal.fr/wp-content/uploads/sites/17/2021/01/max-verstappen-jet-prive-f1.jpg"
+    ],
+
+    'produit_3' => [
+        'nom' =>"avion3",
+        'prix' => 200 . 'eur par j',
+        'poid' => 5 .'personnes' ,
+        'remise' => null,
+        'photo' => "https://img.pixers.pics/pho_wat(s3:700/FO/34/09/39/49/700_FO34093949_fb4ccc15b282a351ebefd19318f31c16.jpg,700,465,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,415,jpg)/papiers-peints-jet-prive.jpg.jpg",
+    ],
+
+];
+
+
+foreach ($array as $prod) {
+    foreach ($prod as $info){
+        echo $info . '<br/>';
+    }
+
+}
+?>
