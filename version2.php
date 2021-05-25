6f838ca1be7cb8d1e31e1c6cf7804a2eaee41502
Wissem CHAHED <wissem.chahed@le-campus-numerique.fr>
2021-05-25 17:05:28 +0200

    version2 avec toute les boucle

--- version2.php
+++ version2.php
@@ -26,39 +26,101 @@ $array = [
     ],
 
 ];
+
+
+
+
+//foreach ($array as $key => $prod) {
+//    echo $key.' : <br/>';
+//    foreach ($prod as $key2 => $prod2){
+//        if ($key2 === 'photo') {
+//            echo $key2." :  <img src='".$prod2."' /><br />";
+//        } else {
+//            echo $key2." :  ".$prod2.'<br />';
+//        }
+//    }
+//}
+//$keys = array_keys($array);
+//for($i=0; $i < count($keys); ++$i) {
+//    echo $keys[$i] .' : <br/>';
+//    $array2 = $array[$keys[$i]];
+//    $keys2 = array_keys($array2);
+//    for($j=0; $j < count($keys2); ++$j) {
+//        if ($keys2[$j] === 'photo') {
+//            echo $keys2[$j]." :  <img src='".$array2[$keys2[$j]]."' width='200' height='auto' /><br />";
+//        } else {
+//            echo $keys2[$j]." :  ".$array2[$keys2[$j]].'<br />';
+//        }
+//    }
+//}
+
+//$i = 0;
+//do {echo $array[$i]['produit']['nom'].'<br />';
+//    echo $array[$i]['produit']['prix'].'<br />';
+//    echo $array[$i]['produit']['poid'].'<br />';
+//    echo $array[$i]['produit']['remis'].'<br />';
+//    echo ' <img src="'.$array[$i]['produit']['photo'].' width="200" height="auto"><br />';
+//    $i++;
+//
+//
+//}
+//
+//while ($i < count($array));
+//$i++;
+//
+//
+$i=0;
+$keys = array_keys($array);
+while ($i<count($array) ){
+    echo $array[$i]['produit']['nom'].'<br />';
+    echo $array[$i]['produit']['prix'].'<br />';
+    echo $array[$i]['produit']['poid'].'<br />';
+    echo $array[$i]['produit']['remis'].'<br />';
+    echo ' <img src="'.$array[$i]['produit']['photo'].'"><br />';
+    $i++;
+}
+
+
 ?>
 
-<html lang="fr">
-<?php  include 'header.php'; ?>
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col align-self-start">
-            <?php echo $array["produit_1"] ["nom"]?>
-            <?php echo $array["produit_1"] ["prix"]?> eur par journee
-            <?php echo $array["produit_1"] ["poid"]?>  personnes
-            <?php echo $array["produit_1"] ["remise"]?>
-            <?php echo '<img src="'.$array["produit_1"] ["photo"].'" alt="votre avion" width="200">'; ?>
-        </div>
-        <div class="col align-self-center">
-            <?php echo $array["produit_2"] ["nom"]?>
-            <?php echo $array["produit_2"] ["prix"]?> eur par journee
-            <?php echo $array["produit_2"] ["poid"]?>  personnes
-            <?php echo $array["produit_2"] ["remise"]?>
-            <?php echo '<img src="'.$array["produit_2"] ["photo"].'" alt="votre avion" width="300">'; ?>
 
-        </div>
-        <div class="col align-self-end">
-            <?php echo $array["produit_3"] ["nom"]?>
-            <?php echo $array["produit_3"] ["prix"]?> eur par journee
-            <?php echo $array["produit_3"] ["poid"]?>  personnes
-            <?php echo $array["produit_3"] ["remise"]?>
-            <?php echo '<img src="'.$array["produit_3"] ["photo"].'" alt="votre avion" width="300">'; ?>
 
-        </div>
-    </div>
-</div>
 
-</body>
-<?php  include 'footer.php'; ?>
-</html>
+
+
+
+<!---->
+<!--<html lang="fr">-->
+<?php // include 'header.php'; ?>
+<!--<body>-->
+<!--<div class="container">-->
+<!--    <div class="row">-->
+<!--        <div class="col align-self-start">-->
+<!--            --><?php //echo $array["produit_1"] ["nom"]?>
+<!--            --><?php //echo $array["produit_1"] ["prix"]?><!-- eur par journee-->
+<!--            --><?php //echo $array["produit_1"] ["poid"]?><!--  personnes-->
+<!--            --><?php //echo $array["produit_1"] ["remise"]?>
+<!--            --><?php //echo '<img src="'.$array["produit_1"] ["photo"].'" alt="votre avion" width="200">'; ?>
+<!--        </div>-->
+<!--        <div class="col align-self-center">-->
+<!--            --><?php //echo $array["produit_2"] ["nom"]?>
+<!--            --><?php //echo $array["produit_2"] ["prix"]?><!-- eur par journee-->
+<!--            --><?php //echo $array["produit_2"] ["poid"]?><!--  personnes-->
+<!--            --><?php //echo $array["produit_2"] ["remise"]?>
+<!--            --><?php //echo '<img src="'.$array["produit_2"] ["photo"].'" alt="votre avion" width="300">'; ?>
+<!---->
+<!--        </div>-->
+<!--        <div class="col align-self-end">-->
+<!--            --><?php //echo $array["produit_3"] ["nom"]?>
+<!--            --><?php //echo $array["produit_3"] ["prix"]?><!-- eur par journee-->
+<!--            --><?php //echo $array["produit_3"] ["poid"]?><!--  personnes-->
+<!--            --><?php //echo $array["produit_3"] ["remise"]?>
+<!--            --><?php //echo '<img src="'.$array["produit_3"] ["photo"].'" alt="votre avion" width="300">'; ?>
+<!---->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!---->
+<!--</body>-->
+<?php // include 'footer.php'; ?>
+<!--</html>-->
