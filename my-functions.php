4078dec2479b4448b862ec287db4ed7b2d4f0d0e
wissem <wissem-chahed@le-campus-numerique.fr>
2021-05-26 11:14:31 +0200

    toutes les fonctions

--- my-functions.php
+++ my-functions.php
@@ -1,1 +1,26 @@
 <?php
+function formatPrice($price)
+{
+    $newPrice = number_format($price,2);
+    echo $newPrice. ' €';
+
+
+}
+
+
+function priceExcludingVAT($prix)
+{
+    $newPrice = (100 * $prix) / (100 + 20);
+    formatPrice($newPrice);
+}
+
+
+function displayDiscountedPrice($price,$remise)
+{
+    $promo = $price-($price/100 *$remise);
+    formatPrice($promo);
+}
+?>
+
+
+
