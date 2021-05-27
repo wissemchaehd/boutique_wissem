3e1cb65fa346dfefa02beec36973738ec62584b1
wissem <wissem-chahed@le-campus-numerique.fr>
2021-05-27 20:19:17 +0200

    cart.php

--- cart.php
+++ cart.php
@@ -1,1 +1,23 @@
+<html>
+<meta charset="UTF-8"/>
 <?php
+include 'header.php';
+include 'my-functions.php';
+?>
+<body>
+<table>
+    <tr>
+        <td style="border: solid"> Produit </td>
+        <td style="border: solid"> prix unitaire  </td>
+        <td style="border: solid"> Quantité </td>
+        <td style="border: solid"> Total </td>
+    </tr>
+
+    <tr>
+        <td style="border: solid"><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
+        <td style="border: solid"><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
+        <td style="border: solid"><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
+        <td style="border: solid"><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
+    <tr/>
+</body>
+</html>
