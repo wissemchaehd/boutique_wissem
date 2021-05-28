b6f32a86974fca70754e826dc255f3032310dfbe
Wissem CHAHED <wissem.chahed@le-campus-numerique.fr>
2021-05-28 16:42:36 +0200

    cart ballon

--- cart.php
+++ cart.php
@@ -1,41 +1,41 @@
 <html>
 <meta charset="UTF-8"/>
 <?php
-include 'header.php';
+
 
 ?>
 <body>
 
 <table class="table table-hover">
     <tr>
-        <td style="color: #a48e1f"> Produit </td>
-        <td style="color: #a48e1f"> prix unitaire  </td>
-        <td style="color: #a48e1f"> Quantité </td>
-        <td style="color: #a48e1f"> Total </td>
+        <td> Produit </td>
+        <td> prix unitaire  </td>
+        <td> Quantité </td>
+        <td> Total </td>
     </tr>
 
     <tr>
-        <td style="color: #a48e1f"><?php echo htmlspecialchars($_POST['nom']) ?></td>
-        <td style="color: #a48e1f"><?php echo(int) htmlspecialchars($_POST['prix']) ?></td>
-        <td style="color: #a48e1f"><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
+        <td><?php echo htmlspecialchars($_POST['nom']) ?></td>
+        <td><?php echo(int) htmlspecialchars($_POST['prix']) ?></td>
+        <td><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
     <tr/>
 
     <tr>
         <td </td>
         <td </td>
-        <td style="color: #a48e1f"> Total HT </td>
+        <td> Total HT </td>
 
     </tr>
     <tr>
         <td </td>
         <td </td>
-        <td style="color: #a48e1f"> TVA </td>
+        <td> TVA </td>
 
     </tr>
     <tr>
         <td </td>
         <td </td>
-        <td style="color: #a48e1f">Total TTC </td>
+        <td>Total TTC </td>
 
     </tr>
 
