7f25dd9220cc7016bb0fd5b3a2c83b3acd2408df
wissem <wissem-chahed@le-campus-numerique.fr>
2021-05-30 16:26:27 +0200

    cart new

--- cart.php
+++ cart.php
@@ -1,43 +1,65 @@
 <html>
 <meta charset="UTF-8"/>
 <?php
-
-
+include 'my-functions.php';
 ?>
 <body>
 
-<table class="table table-hover">
+<table>
     <tr>
-        <td> Produit </td>
-        <td> prix unitaire  </td>
-        <td> Quantité </td>
-        <td> Total </td>
+        <td style="padding-right: 30%"> Produit </td>
+        <td style="padding-left: 10%"> Prix unitaire  </td>
+        <td style="padding-left :10%"> Quantité </td>
+        <td style="padding-left :10%"> Total </td>
     </tr>
 
     <tr>
         <td><?php echo htmlspecialchars($_POST['nom']) ?></td>
-        <td><?php echo(int) htmlspecialchars($_POST['prix']) ?></td>
-        <td><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
+        <td style="padding-left: 10%"><?php echo(int) htmlspecialchars($_POST['prix']). ' €' ?></td>
+        <td style="padding-left: 10%"><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
+        <td style="padding-left: 10%"><?php echo(int) Total($_POST['prix'],$_POST['quantité']). ' €' ?></td>
     <tr/>
 
     <tr>
-        <td </td>
-        <td </td>
-        <td> Total HT </td>
+        <td> </td>
+        <td> </td>
+        <td style="padding-left: 10%"> Total HT </td>
+        <td style="padding-left: 10%"><?php echo(int) Total($_POST['PrixHT'],$_POST['quantité']). ' €'?></td>
+
 
     </tr>
     <tr>
-        <td </td>
-        <td </td>
-        <td> TVA </td>
+        <td> </td>
+        <td> </td>
+        <td style="padding-left: 10%"> TVA </td>
+        <td style="padding-left: 10%"><?php echo(int) TVA($_POST['prix'],$_POST['PrixHT'],$_POST['quantité']). ' €' ?></td>
 
     </tr>
     <tr>
-        <td </td>
-        <td </td>
-        <td>Total TTC </td>
+        <td> <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                <option selected>Choix du Transporteur</option>
+                <option value="1">Transporteur 1</option>
+                <option value="2">Tranporteur 2</option>
+            </select>
+            <button type="submit" > Valider </button>
+        </td>
+    </tr>
+   
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td style="padding-left:10%">Total TTC </td>
+        <td style="padding-left: 10%"><?php echo(int) Total($_POST['prix'],$_POST['quantité']). ' €' ?></td>
 
     </tr>
+    <tr>
+        <td> </td>
+        <td> </td>
+        <td style="padding-left:10%">Transport </td>
+        <td style="padding-left: 10%"><?php echo(int) Total($_POST['prix'],$_POST['quantité']). ' €' ?></td>
+
+    </tr>
+
 
 
 
