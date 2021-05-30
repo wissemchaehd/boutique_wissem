8c77baab19c53ffcbf97b67ed9f137b3ff330ea3
wissem <wissem-chahed@le-campus-numerique.fr>
2021-05-30 21:25:21 +0200

    functi trans

--- my-functions.php
+++ my-functions.php
@@ -11,25 +11,51 @@ function formatPrice($price)
 function priceExcludingVAT($price)
 {
     $newPrice1 =number_format($price*100 / 120,2);
-    return($newPrice1). ' €';
+    return($newPrice1);
 }
 
 
 function displayDiscountedPrice($price,$remise)
 {
     $promo = number_format($price-($price/100 *$remise),2);
-    return($promo). ' €';
+    return($promo);
 }
 
 function  Total($price,$quantite){
     $totals = number_format($price * $quantite,2);
-    return($totals). ' €';
+    return($totals);
 
 }
 function TVA($price,$price1,$quantite){
     $tva =  number_format($price-$price1,2)* $quantite;
 
     return ($tva);
+}
+
+
+function transport($poid,$quatité)
+{
+    $x = $_POST['poid']*$_POST['quantité'];
+//echo $x;
+    $fraisdeport = 5;
+    if( 0 < $x & $x< 500 )
+    {
+        //$fraisdeport = 5;
+        //echo $fraisdeport;
+
+    }
+    elseif (501<$x & $x<2000)
+    {
+        $fraisdeport = 0.9 * ($fraisdeport * ceil($x/500));
+        //echo $fraisdeport;
+        //echo ceil($x/500);
+    }
+    else
+    {
+        $fraisdeport = 0;
+        //echo $fraisdeport;
+    }
+    return $fraisdeport;
 }
 ?>
 
