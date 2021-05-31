1ffe55f7ddd9c398d1681592945da43a24708430
Wissem CHAHED <wissem.chahed@le-campus-numerique.fr>
2021-05-31 09:16:44 +0200

    pas le bon

--- cart.php
+++ cart.php
@@ -1,9 +1,6 @@
 <html>
 <meta charset="UTF-8"/>
-<?php
 
-
-?>
 <body>
 
 <table class="table table-hover">
@@ -13,34 +10,26 @@
         <td> Quantité </td>
         <td> Total </td>
     </tr>
-
     <tr>
         <td><?php echo htmlspecialchars($_POST['nom']) ?></td>
         <td><?php echo(int) htmlspecialchars($_POST['prix']) ?></td>
         <td><?php echo(int) htmlspecialchars($_POST['quantité']) ?></td>
     <tr/>
-
     <tr>
         <td </td>
         <td </td>
         <td> Total HT </td>
-
     </tr>
     <tr>
         <td </td>
         <td </td>
         <td> TVA </td>
-
     </tr>
     <tr>
         <td </td>
         <td </td>
         <td>Total TTC </td>
-
     </tr>
-
-
-
-
+</table>
 </body>
 </html>
