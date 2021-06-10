f3d4a1475891d6c3360b1e7ded32f6b06c34388b
Wissem CHAHED <wissem.chahed@le-campus-numerique.fr>
2021-06-10 16:57:15 +0200

    article le bon

--- Article.php
+++ Article.php
@@ -14,23 +14,22 @@ class Article{
         $this-> id = $id;
         $this-> name = $name;
         $this->Description = $description;
-        $this->Prix = $price;
+        $this->price = $price;
+        $this->weight= $weight;
         $this->image = $image;
-        $this-> id = $id;
         $this->quantity= $quantity;
         $this->available = $available;
         $this->categorie_id = $categorie_id;
     }
     public function displayArticle(){
 
-        echo  'Nom :'.$this->Nom.'<br/>';
+        echo  'Nom :'.$this->name.'<br/>';
         echo  'Description :'.$this->Description.'<br/>';
-        echo  'Prix :'.$this->Prix.'<br/>';
+        echo  'Prix :'.$this->price.'<br/>';
         echo  'Image :'.'<img src='." $this->image ". 'alt="" width="200">'.'<br/>';
-        echo  'poid :'.$this->Poids.'<br/>';
-        echo  'Disponible'.$this->Disponible.'<br/>';
+        echo  'poid :'.$this->weight.'<br/>';
+        echo  'Disponible'.$this->available.'<br/>';
     }
 
 
 }
-
